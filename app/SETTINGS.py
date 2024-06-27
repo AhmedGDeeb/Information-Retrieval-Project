7705129290d90922980fb6f7d6dfc7bb1d6246e1
AhmedGDeeb <94413225+AhmedGDeeb@users.noreply.github.com>
2024-06-27 13:00:42 +0300

    Add files via upload

---
+++ app/SETTINGS.py
@@ -0,0 +1,5 @@
+
+
+UPLOAD_FOLDER = 'C:/pyvm-SIR601/code/app/data'
+ALLOWED_EXTENSIONS = {'docx', 'pptx', 'xls', 'xlsx', 'txt', 'pdf', 'epub', 'csv', 'rtf', 'ptx'}
+MAX_CONTENT_LENGTH = 16 * 1000 * 1000
